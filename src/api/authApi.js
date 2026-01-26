25a5d046eb4370b6976dd5978fb5f51a1749e6b2
nguyen khanh hung <nguyenkhanhhung1531@gmail.com>
2026-01-27 01:27:37 +0700

    fix: thêm quen pass

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,6 +1,6 @@
 import apiClient from './axiosConfig';
 
-// 1. Đăng nhập - PHẢI CÓ /auth
+// 1. Đăng nhập (PHẢI CÓ /auth)
 export const login = async (credentials) => {
     try {
         const response = await apiClient.post('/auth/login', credentials);
@@ -9,11 +9,22 @@ export const login = async (credentials) => {
         throw new Error(error.response?.data?.message || "Sai tài khoản hoặc mật khẩu"); 
     }
 };
+// 5. Admin - Lấy danh sách users
+export const getAllUsersAdmin = async () => {
+    try {
+        const response = await apiClient.get('/admin/users');
+        return response.data;
+    } catch (error) {
+        throw new Error(
+            error.response?.data?.message || "Không thể lấy danh sách người dùng"
+        );
+    }
+};
 
-// 2. Đăng ký - PHẢI CÓ /auth
+// 2. Đăng ký (PHẢI CÓ /auth)
 export const register = async (userData) => {
     try {
-        const finalData = { ...userData, device_id: userData.device_id || "ID_TEST" };
+        const finalData = { ...userData, device_id: userData.device_id || "ID_TEST_CUNG" };
         const response = await apiClient.post('/auth/register', finalData);
         return response.data;
     } catch (error) {
@@ -21,10 +32,11 @@ export const register = async (userData) => {
     }
 };
 
-// 3. Quên mật khẩu - Gửi OTP
+// 3. Quên mật khẩu - Gửi OTP (Hàm này ní đang làm)
 export const sendOtpApi = async (email) => {
     try {
         console.log("🚀 Đang gọi API gửi OTP cho:", email);
+        // Khớp 100%: /api + /auth + /forgot-password
         const response = await apiClient.post('/auth/forgot-password', { email }); 
         return response.data;
     } catch (error) {
@@ -33,7 +45,7 @@ export const sendOtpApi = async (email) => {
     }
 };
 
-// 4. Reset mật khẩu - PHẢI CÓ /auth
+// 4. Reset mật khẩu (PHẢI CÓ /auth)
 export const resetPasswordApi = async (data) => {
     try {
         const response = await apiClient.post('/auth/reset-password', data);
@@ -41,4 +53,5 @@ export const resetPasswordApi = async (data) => {
     } catch (error) {
         throw new Error(error.response?.data?.message || "Mã OTP không đúng");
     }
+    
 };
