e17a2f9807d199439f6e11a880143239bde8d39e
nguyen khanh hung <nguyenkhanhhung1531@gmail.com>
2026-01-27 00:49:43 +0700

    fix: thêm new

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -59,7 +59,7 @@ const Login = () => {
       alignItems: 'center', 
       minHeight: '100vh',
       padding: '20px',
-      background: `linear-gradient(rgba(0, 21, 41, 0.7), rgba(0, 21, 41, 0.7)), url('https://images.unsplash.com/photo-1550684848-fac1c5b4e853?auto=format&fit=crop&q=80&w=2070')`,
+      background: `linear-gradient(rgba(0, 21, 41, 0.7), rgba(0, 21, 41, 0.7)), url('https://lifeinitaly.com/wp-content/uploads/2018/08/676px-Self-portrait_as_the_Allegory_of_Painting_by_Artemisia_Gentileschi.jpg')`,
       backgroundSize: 'cover',
       backgroundPosition: 'center',
     }}>
